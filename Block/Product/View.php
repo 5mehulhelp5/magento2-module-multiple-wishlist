50ae1e7d4b8f9d5d9ccc89219e37918b7660723a
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2026-09-17 08:23:52 -0300

    ✨ Implementing the wishlist add action

--- Block/Product/View.php
+++ Block/Product/View.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @category    BrunoDuarte
+ * @package     BrunoDuarte_MultipleWishlist
+ * @copyright   Copyright (c) 2026 BrunoDuarte
+ */
+
+declare(strict_types=1);
 
 namespace BrunoDuarte\MultipleWishlist\Block\Product;
 
