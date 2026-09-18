67ec8c6778a12e8f00e0cf2148ce90b65013a080
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2026-09-18 08:22:43 -0300

    ✨ Implementing function display block for user logged

--- Block/Product/View.php
+++ Block/Product/View.php
@@ -9,9 +9,33 @@ declare(strict_types=1);
 
 namespace BrunoDuarte\MultipleWishlist\Block\Product;
 
+use BrunoDuarte\MultipleWishlist\Helper\Data as DataHelper;
 use Magento\Framework\View\Element\Template as ElementTemplate;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\Model\StoreManagerInterface;
 
 class View extends ElementTemplate
 {
+    public DataHelper $dataHelper;
+    private StoreManagerInterface $storeManager;
 
+    public function __construct(
+        Context $context,
+        DataHelper $dataHelper,
+        StoreManagerInterface $storeManager
+    ) {
+        parent::__construct($context);
+        $this->dataHelper = $dataHelper;
+        $this->storeManager = $storeManager;
+    }
+
+    public function canDisplayBlock()
+    {
+        return false;
+    }
+
+    protected function getStoreId(): int
+    {
+        return (int) $this->storeManager->getStore()->getId();
+    }
 }
