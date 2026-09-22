e4154000d50031d563e95bc71a8c8af98e13e1c6
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2026-09-22 09:14:22 -0300

    ✨ Display of the block on the product page and wishlist in the selection options.

--- Block/Product/View.php
+++ Block/Product/View.php
@@ -10,6 +10,9 @@ declare(strict_types=1);
 namespace BrunoDuarte\MultipleWishlist\Block\Product;
 
 use BrunoDuarte\MultipleWishlist\Helper\Data as DataHelper;
+use BrunoDuarte\MultipleWishlist\Model\ResourceModel\MultipleWishlist\CollectionFactory as MultipleWishlistCollectionFactory;
+use BrunoDuarte\MultipleWishlist\Model\ResourceModel\MultipleWishlist\Collection as MultipleWishlistCollection;
+use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\View\Element\Template as ElementTemplate;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Store\Model\StoreManagerInterface;
@@ -18,21 +21,49 @@ class View extends ElementTemplate
 {
     public DataHelper $dataHelper;
     private StoreManagerInterface $storeManager;
+    protected CustomerSession $customerSession;
+    protected MultipleWishlistCollectionFactory $multipleWishlistCollectionFactory;
 
     public function __construct(
         Context $context,
         DataHelper $dataHelper,
-        StoreManagerInterface $storeManager
+        StoreManagerInterface $storeManager,
+        CustomerSession $customerSession,
+        MultipleWishlistCollectionFactory $multipleWishlistCollectionFactory
     ) {
         parent::__construct($context);
         $this->dataHelper = $dataHelper;
         $this->storeManager = $storeManager;
+        $this->customerSession = $customerSession;
+        $this->multipleWishlistCollectionFactory = $multipleWishlistCollectionFactory;
     }
 
-    public function canDisplayBlock()
+    /**
+     * Tests whether or not the block can be displayed.
+     *
+     * @return bool
+     */
+    public function canDisplayBlock(): bool
     {
-        return false;
+        if (!$this->dataHelper->isModuleEnable() || !$this->customerSession->isLoggedIn()) {
+            return false;
+        }
+
+        return true;
     }
+
+    /**
+     * Get a multiple wishlist list active order by title.
+     *
+     * @return MultipleWishlistCollection
+     */
+    public function getMultipleWishlistList()
+    {
+        $wishlistActiveCollection = $this->multipleWishlistCollectionFactory->create();
+        $wishlistActiveCollection->addFieldToFilter('is_active', '1')->addOrder('title', 'asc');
+        return $wishlistActiveCollection;
+    }
+
 
     protected function getStoreId(): int
     {
